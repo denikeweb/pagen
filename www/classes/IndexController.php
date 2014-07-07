042c66619956ffa5911af335e052bf1a63c6319c
Denis <denikewebpost@yandex.ua>
2014-07-07 06:22:32 +0300

    +

--- www/classes/IndexController.php
+++ www/classes/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends eController {
 		$data ['title'] = Site::$ThisPage['title'];
 		$data ['meta_d'] = Site::$ThisPage['meta_d'];
 		$data ['meta_k'] = Site::$ThisPage['meta_k'];
-		//$data ['info'] = Site::$ThisPage['info'];
+		$data ['info'] = Site::$ThisPage['info'];
 		$data ['content'] = Site::$Content;
 
 		$this->loadModel ('_design');
